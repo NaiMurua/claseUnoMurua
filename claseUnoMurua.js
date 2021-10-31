bdfc16528a34d441123cf43e1ca82975f6eb3706
NaiMurua <35934275+NaiMurua@users.noreply.github.com>
2021-10-30 23:10:03 -0300

    Add files via upload

---
+++ claseUnoMurua.js
@@ -0,0 +1,26 @@
+
+
+let nombreUsuario = prompt("Ingrese el nombre de su Personaje favorito : ");
+let colorUsuario = prompt("Ingrese el nombre de su color favorito:");
+let numero = prompt("Ingrese un numero:");
+let usuarioYear = parseInt(prompt("Ingrese el año de su nacimiento:"));
+let usuarioMes = parseInt(prompt("Ingrese el nro de mes de su nacimiento:"));
+let usuarioDia = parseInt(prompt("Ingrese el nro de dia de su nacimiento: "));
+
+let con = colorUsuario + numero + nombreUsuario;
+let suma = usuarioDia + usuarioMes + usuarioYear;
+
+
+alert("Su nuevo PASSWORD TEMPORAL para navegar en el sitio es : " + suma);
+alert("Su nuevo USUARIO TEMPORAL para navegar en el sitio es :  " + con);
+
+console.log("FIN DEL INGRESO DE DATOS");
+
+
+
+
+
+
+
+
+
