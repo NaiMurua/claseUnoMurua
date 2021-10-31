111a1c5358cd5da4a276c4bec820b972c12e9f81
NaiMurua <35934275+NaiMurua@users.noreply.github.com>
2021-10-30 23:12:32 -0300

    Update claseUnoMurua.js

--- claseUnoMurua.js
+++ claseUnoMurua.js
@@ -10,9 +10,8 @@ let usuarioDia = parseInt(prompt("Ingrese el nro de dia de su nacimiento: "));
 let con = colorUsuario + numero + nombreUsuario;
 let suma = usuarioDia + usuarioMes + usuarioYear;
 
-
-alert("Su nuevo PASSWORD TEMPORAL para navegar en el sitio es : " + suma);
 alert("Su nuevo USUARIO TEMPORAL para navegar en el sitio es :  " + con);
+alert("Su nuevo PASSWORD TEMPORAL para navegar en el sitio es : " + suma);
 
 console.log("FIN DEL INGRESO DE DATOS");
 
